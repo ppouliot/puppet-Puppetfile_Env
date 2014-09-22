77a8425e2f1b53a075a06355a3530d4d2501e2f3
Peter J. Pouliot <peter@pouliot.net>
2014-09-22 17:56:08 -0400

    updating scripts

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -10,6 +10,9 @@ cd /etc/
 echo -n "- Retrieving Base /etc/puppet"
 git clone https://github.com/ppouliot/puppet-etc_puppet puppet
 
+echo -n "installing base puppetfile for /etc/puppet/modules"
+wget -cv https://raw.githubusercontent.com/ppouliot/puppet-Puppetfile_Env/master/Puppetfile.base -O /etc/puppet/Puppetfile
+
 echo -n "- Installing PuppetLabs Repositories"
 cd /tmp
 # this is a debian/ubuntu specific command
@@ -30,22 +33,6 @@ gem install r10k
 
 
 echo -n "- Installing Puppetlabs Supported and Endorsed Modules to BaseModulePath'
-#cd /etc/puppet && r10k --verbose DEBUG puppetfile install
-
-
-#echo "### Retrieving Puppetfile from Openstack-Hyper-V ####"
-#cd /usr/local/src
-#git clone https://github.com/openstack-hyper-v/Puppetfile-cambridge
-#cd Puppetfile-cambridge
-#./install_puppetfile.sh
-
-#echo "### RUNNING R10K ###"
-#cd /etc/puppet && r10k --verbose DEBUG puppetfile install
-
-#Setting Minimal hiera for QuarterMaster
-#cp -R /etc/puppet/modules/quartermaster/files/hiera /etc/puppet/hiera
-#echo "creating symlink for from ./hiera.yaml to /etc/puppet/hiera.yaml"
-#ln -s /etc/puppet/hiera/hiera.yaml /etc/puppet/hiera.yaml
-
-#echo "### BOOTSTRAPPING QUARTERMASTER ###"
-#puppet apply --verbose --trace --debug --modulepath=/etc/puppet/modules /etc/puppet/modules/quartermaster/tests/init.pp
+cd /etc/puppet && r10k --verbose DEBUG puppetfile install
+echo -n "- deploying environment modules'
+cd /etc/puppet && r10k --verbose DEBUG deploy environment -vp
